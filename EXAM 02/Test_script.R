8151a152439a2e979f12fde25da04f27a05b145b
Mauricio Vazquez <mvazq109@itam.mx>
2024-11-24 13:21:27 -0600

    Analisis univariado

--- EXAM 02/Test_script.R
+++ EXAM 02/Test_script.R
@@ -1,2 +1,10 @@
 ###### Pregunta 1 ######
 
+# Ruta al archivo
+file_path <- "C:\\Users\\mauva\\OneDrive\\Documents\\ITAM\\9no Semestre\\METODOS MULTIVARIADOS\\REPOSITORIO\\Multivariate_Statistical_Course_Assignments_Fall2024\\EXAM 02\\turtles.rda"
+
+# Carga de los datos
+load(file_path)
+
+# Revision de los datos
+head(turtles)
